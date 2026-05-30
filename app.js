25ae7aecd2de45996f9697e599bdd9f209740738
laibamohsin7 <laibamohsin777@gmail.com>
2026-05-30 13:16:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,107 @@
+
+//2 NUMBER EVEN OR ODD----------
+// num = Number(prompt("enter your number"));
+
+//if (num % 2 === 0) {
+//    document.write(num + " is even");
+//}else{
+//    document.write(num + " is odd");
+ //} 
+
+
+ //3 ELIGIBLE FOR VOTING OR NOT----------
+ //for num = Number(prompt("enter your age"));
+
+ //if(age>= 18) {
+   // document.write("eligible for voting");
+ //}else{ 
+  //  document.write("not eligible for voting");
+ //}
+
+
+ //4
+// numbers = [10, 20, 33, 40, 50];
+
+//var sum = 0; //variable to store sum
+
+//for (let i = 0; i < numbers.length; i++) {
+  //  sum = sum + numbers[i];
+//}
+
+//document.write("Sum of numbers = " + sum);
+
+
+
+//5 palindrome
+
+
+
+
+//7
+//for (let i = 2; i <= 20; i += 2 ) {
+//    document.write(i);
+//}
+
+
+//8 factorial
+
+
+
+//9
+//var names = ["ayesha", "hafsa", "Ahmed", "Zain", "Alishba"];
+
+//names.reverse();
+
+//for (let name of names) {
+//    document.write(name + "<br>");
+//}
+
+
+//10
+//for password = prompt("Enter your password:");
+
+//if (password.length < 8) {
+  //  document.write("Weak password");
+//} else {
+ //   document.write("Strong password");
+// }
+
+
+
+//var num = 6
+
+//for(let i=1; i <=10; i++ ){
+  //document.write(i * num + "<br>" );
+//}
+
+//print even num from 1 to 50----------
+//for (let i = 2; i <= 50; i += 2) {  
+//
+//    document.writeln(i);
+//}
+
+
+
+//number positive or negative------------
+// num = +prompt("Enter a number:");
+
+//if (num > 0) {
+//    ("Positive Number");
+//} else if (num < 0) {
+//    ("Negative Number");
+//} 
+
+// GREATER NUMBER BW 2 NUMS----------
+// num1 = +prompt("Enter first number:");
+// num2 = +prompt("Enter second number:");
+
+//if (num1 > num2) {
+//    (num1 + " is greater");
+//} else if (num2 > num1) {
+//    (num2 + " is greater");
+//}
+
+//ARRAYY
+//var students = ["ali" , "sara" , "zain" , "laiba" , "hafsa"];
+
+// document.writeln(students);
